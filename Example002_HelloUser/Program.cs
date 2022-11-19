1ca110229eb69275459a55bc109f741241c44b2e
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-19 17:24:43 +0300

    Examples002

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -3,7 +3,13 @@
 // Console.WriteLine("Привет, ");
 // Console.WriteLine(username);
 
-Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя ");
+        string username = Console.ReadLine();
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
+}
