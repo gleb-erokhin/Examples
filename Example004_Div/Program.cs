4383fe26270fe4eb2198959c62e4272e5fe07286
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-19 21:42:03 +0300

    Example004_Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,5 +1,12 @@
 ﻿// int numberA = 10;
 // int numberB = 5;
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
+
