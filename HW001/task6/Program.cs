779ec072e0849a1e3d90a4f45bd0d01d03563b78
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-21 22:23:45 +0300

    delete comment line in console code

--- HW001/task6/Program.cs
+++ HW001/task6/Program.cs
@@ -3,8 +3,8 @@
 // -3 -> нет
 // 7 -> нет
 
-// Console.WriteLine("Введите число ");
-// int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
 
 if (num % 2 == 0)
 {
