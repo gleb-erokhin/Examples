d5c695baf8a8378c18f9eccc92a20c51c9e866bf
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-21 22:12:31 +0300

    added new tasks, mouve from other folder

---
+++ HW001/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число А ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine($"max = {numA}");
+}
+else
+{
+    Console.WriteLine($"max = {numB}");
+}
---
+++ HW001/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число А ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB && numA > numC)
+{
+    Console.WriteLine($"max = {numA}");
+}
+else if (numB > numA && numB > numC)
+{
+    Console.WriteLine($"max = {numB}");
+}
+else
+{
+    Console.WriteLine($"max = {numC}");
+}
