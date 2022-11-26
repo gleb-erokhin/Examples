fe1165c299f845ea7249999bdad71d6157062344
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-26 21:25:18 +0300

    task15 is done

--- HW002/task013/Program.cs
+++ HW002/task013/Program.cs
@@ -4,16 +4,14 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// Console.WriteLine("Введите число ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int result = num % 10;
-// Console.WriteLine(result);
-
-
-int n = 78;
-while (n > 1000) n /= 10;
-Console.WriteLine($"{n % 10}");
-
-
-
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 99)
+{
+    Console.WriteLine($"Нет третьей цифры");
+}
+else
+{
+    while (num > 1000) num /= 10;
+    Console.WriteLine($"{num % 10}");
+}
