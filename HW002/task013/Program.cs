d5532f3d877541ac8b38e2140583271f30f185c9
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-26 19:59:25 +0300

    task015 is done

---
+++ HW002/task013/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num % 10;
+// Console.WriteLine(result);
+
+
+int n = 78;
+while (n > 1000) n /= 10;
+Console.WriteLine($"{n % 10}");
+
+
+
+// int n = Convert.ToInt32(Console.ReadLine());
