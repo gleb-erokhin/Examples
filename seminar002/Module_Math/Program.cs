d2f37e174462612cdc7848c026c0fc85e2a5302d
gleb-erokhin <gleb.erokhin@gmail.com>
2022-11-26 10:21:36 +0300

    semenar002 math

---
+++ seminar002/Module_Math/Program.cs
@@ -0,0 +1,11 @@
+﻿// Математические операции
+// int a = -4;
+double a = 2.22222;
+int b = 3;
+
+// int result = Math.Abs(a); // модуль числа из библитеки математики
+// int result = Math.Max(a, b); // max min из двух значений
+// double result = Math.Pow(a, b); // возвести в степень
+double result = Math.Round(a, 2); // округление, второе значение количество знаков после запятой
+
+Console.WriteLine(result);
