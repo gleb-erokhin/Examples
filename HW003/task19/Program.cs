3ba8cec7c2f2291b0692cc6c2a510026d18306a5
gleb.erokhin <gleb.erokhin@gmail.com>
2022-12-02 18:01:58 +0300

    task19 new try

--- HW003/task19/Program.cs
+++ HW003/task19/Program.cs
@@ -4,14 +4,37 @@
 // 12821 -> да
 // 23432 -> да
 
-// int[] num =  new int {};
+// int[] num =  new int[1];
 
 // Console.WriteLine("Введите число ");
-// num = Convert.ToInt32(Console.ReadLine());
+// num[0] = Convert.ToInt32(Console.ReadLine());
 
-// foreach (int n in num)
-// {
-//     Console.WriteLine(n);
-// }
+
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+while(num > 0)
+{
+    int digit = num % 10;
+    num /= 10;
+    num2 = num2 * 10 + digit; 
+}
+
+num /= 1000;
+num2 /= 1000;
+Console.WriteLine($"начальное число {num}");
+Console.WriteLine($"измененное число {num2}");
+if (num == num2)
+{
+
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+
 
 
