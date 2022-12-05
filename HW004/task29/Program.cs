51485d2845b4b76a49e92f6d9c10370bcd1475c1
gleb.erokhin <gleb.erokhin@gmail.com>
2022-12-05 18:13:33 +0300

    task25, 27, 29 upgrade

--- HW004/task29/Program.cs
+++ HW004/task29/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arayRandom()
+{
+    int[] array = new int[8];
+    int i = 0;
+    while(i < array.Length)
+    {
+        array[i] = new Random().Next(1, 50);
+        i++; 
+    }
+    return array;
+}
+
+int[] x = arayRandom();
+
+for (int i = 0; i < x.Length; i++)
+Console.Write($"{x[i]}, ");
+
+
