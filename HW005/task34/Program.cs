fb5daa87a689e221d2f0c9b3adbe2ee4366d68a5
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-07 22:37:32 +0300

    task 34 in progress

--- HW005/task34/Program.cs
+++ HW005/task34/Program.cs
@@ -1,3 +1,41 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+
+int[] arayRandom()
+{
+    int[] array = new int[4];
+    int i = 0;
+    while(i < array.Length)
+    {
+        array[i] = new Random().Next(100, 1000);
+        i++; 
+    }
+    return array;
+}
+
+int ShowNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] x = arayRandom();
+
+// посмотреть урок преподавателя по поводу метода принт
+for (int i = 0; i < x.Length; i++)
+Console.Write($"{x[i]}, ");
+
+Console.WriteLine();
+
+int y = ShowNum(x);
+Console.WriteLine($"Четных чисел в массиве = {y}");
