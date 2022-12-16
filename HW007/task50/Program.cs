b7ea7777f134aa97154907310d6d0b07fe447947
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-16 07:37:13 +0300

    task50 52 in progress

---
+++ HW007/task50/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int[,] arrRand()
+{
+    int[,] a = new int[3,4];
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i,j] = new Random().Next(1, 10); 
+        }
+    }
+    return a;
+}
+
+void FindPosition(int num1, int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (num1 == array[i, j])
+            {
+                Console.WriteLine("test");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("net");
+
+            }
+        }
+        
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($" {array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] x = arrRand();
+PrintArray(x);
+FindPosition(8, x);
---
+++ HW007/task52/Program.cs
@@ -0,0 +1,39 @@
+﻿int[,] arrRand()
+{
+    int[,] a = new int[3,4];
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i,j] = new Random().Next(1, 10); 
+        }
+    }
+    return a;
+}
+
+int[,] Average(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i,j] = new Random().Next(1, 10); 
+        }
+    }
+    return a;
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($" {array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] x = arrRand();
+PrintArray(x);
