656bb7a3a787b7158489b8050f62d6a62fa90118
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-16 22:45:04 +0300

    HW006 is done

--- HW006/task43/Program.cs
+++ HW006/task43/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void FindCross(float k1, float k2, float b1, float b2)
+{
+    float x = 0;
+    float y = 0;
+    if (k1 == k2 && b1 != b2) Console.WriteLine("Прямые паралельны");
+    else if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают");
+    else
+    {
+        x = (b1 - b2) / (k2 - k1);
+        y = k1 * (b2 - b1) / (k1 - k2) + b1;
+        Console.WriteLine($"Координаты пересечения прямых ({x}; {y})");
+    }
+    
+}
+
+FindCross(5, 9, 2, 4);
+
