88015709790cdc1310925a9f5461f43fdb0c50a5
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-21 07:07:14 +0300

    update task52

--- HW007/task52/Program.cs
+++ HW007/task52/Program.cs
@@ -22,20 +22,26 @@ int[,] arrRand()
 
 void Average(int[,] a)
 {
-  float sum = 0;
-  float[] res = new float[4];
+  int[] sum = new int[4];
   for (int i = 0; i < a.GetLength(0); i++)
   {
       for (int j = 0; j < a.GetLength(1); j++)
         {
           
-          sum = sum + a[j, i];
-          Console.WriteLine($" 2 for {sum }");
-          Console.WriteLine($" j {j}");
-          Console.WriteLine($" i {i}"); 
+          Console.WriteLine($"before {i}");
+          Console.WriteLine($"before {j}");
+          sum[i] += a[j, i];
+          Console.WriteLine($"after {i}");
+          Console.WriteLine($"after {j}");
+
         }
+        Console.WriteLine();
   }
-  Console.WriteLine($"{sum / a.GetLength(1)}");
+  Console.WriteLine();
+  foreach(float s in sum)
+  {
+    Console.WriteLine(s / 4);
+  }
   
 }
 
