75002429627fc7881a35df6aa3f2207af62f2810
gleb.erokhin <gleb.erokhin@gmail.com>
2022-12-30 19:25:56 +0300

    task54 is runnig

--- HW008/task54/Program.cs
+++ HW008/task54/Program.cs
@@ -8,3 +8,51 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] array = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+// int[,] temp = new int[1, 1];
+// for(int row = 0; row < array.GetLength(0); row++)
+// {
+//     for(int col = 0; col < array.GetLength(1); col++)
+//     {
+//         if(array[row, col] > array[row, col + 1])
+//         {
+//             int k = array[row, col];
+//             array[row, col] = array[row, col + 1];
+//             array[row, col + 1] = k;
+//         }
+//     }
+//     // Console.WriteLine();
+// }
+
+for(int row = 0; row < array.GetLength(0); row++)
+{
+    for(int col = 0; col < array.GetLength(1); col++)
+    {
+        for(int i = 0; i < array.GetLength(1) - 1; i++)
+        {
+            // Console.WriteLine($"(row {row}, col {col})  = {array[row, col]}");
+            if(array[row, col] > array[row, col + 1])
+            {
+                int k = array[row, col];
+                array[row, col] = array[row, col +1];
+                array[row, col + 1] = k;
+            }
+        }
+        // Console.WriteLine($"(row {row}, col + 1 {col + 1}) = {array[row, col + 1]}");
+        // if(array[row, col] > array[row, col + 1])
+        // {
+        //     Console.WriteLine($"col {array[row, col]}");
+        //     Console.WriteLine($"col + 1 {array[row, col + 1]}");
+        // }
+    }
+    // Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetLength(0); i++)  // подсчет размерности массива через метод, (0) в скобках ссылается на строку 12 int[3, 4]
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
