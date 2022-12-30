dc338b0256f460a01a7eb189605999f203a219ab
gleb-erokhin <gleb.erokhin@gmail.com>
2022-12-30 22:00:35 +0300

    task54 done from no random array

--- HW008/task54/Program.cs
+++ HW008/task54/Program.cs
@@ -8,51 +8,64 @@
 // 9 5 3 2
 // 8 4 4 2
 
-int[,] array = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
-// int[,] temp = new int[1, 1];
-// for(int row = 0; row < array.GetLength(0); row++)
-// {
-//     for(int col = 0; col < array.GetLength(1); col++)
-//     {
-//         if(array[row, col] > array[row, col + 1])
-//         {
-//             int k = array[row, col];
-//             array[row, col] = array[row, col + 1];
-//             array[row, col + 1] = k;
-//         }
-//     }
-//     // Console.WriteLine();
-// }
 
 for(int row = 0; row < array.GetLength(0); row++)
 {
     for(int col = 0; col < array.GetLength(1); col++)
     {
-        for(int i = 0; i < array.GetLength(1) - 1; i++)
+        if(array[row, col] > array[row, col + 1])
         {
-            // Console.WriteLine($"(row {row}, col {col})  = {array[row, col]}");
-            if(array[row, col] > array[row, col + 1])
-            {
-                int k = array[row, col];
-                array[row, col] = array[row, col +1];
-                array[row, col + 1] = k;
-            }
+            int k = array[row, col];
+            array[row, col] = array[row, col + 1];
+            array[row, col + 1] = k;
         }
-        // Console.WriteLine($"(row {row}, col + 1 {col + 1}) = {array[row, col + 1]}");
-        // if(array[row, col] > array[row, col + 1])
-        // {
-        //     Console.WriteLine($"col {array[row, col]}");
-        //     Console.WriteLine($"col + 1 {array[row, col + 1]}");
-        // }
     }
     // Console.WriteLine();
 }
-
-for (int i = 0; i < array.GetLength(0); i++)  // подсчет размерности массива через метод, (0) в скобках ссылается на строку 12 int[3, 4]
+int[,] Array(int[,] array)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for(int row = 0; row < array.GetLength(0); row++)
     {
-        Console.Write($"{array[i, j]} ");
+        for(int col = 0; col < array.GetLength(1); col++)
+        {
+            for(int i = 0; i < array.GetLength(1) - 1; i++)
+            {
+                // Console.WriteLine($"(row {row}, col {col})  = {array[row, col]}");
+                if(array[row, i] < array[row, i + 1])
+                {
+                    int k = array[row, i];
+                    array[row, i] = array[row, i +1];
+                    array[row, i + 1] = k;
+                }
+            }
+            // Console.WriteLine($"(row {row}, col + 1 {col + 1}) = {array[row, col + 1]}");
+            // if(array[row, col] > array[row, col + 1])
+            // {
+            //     Console.WriteLine($"col {array[row, col]}");
+            //     Console.WriteLine($"col + 1 {array[row, col + 1]}");
+            // }
+        }
+        // Console.WriteLine();
+    }
+    return array;
+}
+
+void printArr(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)  // подсчет размерности массива через метод, (0) в скобках ссылается на строку 12 int[3, 4]
+{
+    for (int j = 0; j < a.GetLength(1); j++)
+    {
+        Console.Write($"{a[i, j]} ");
     }
     Console.WriteLine();
 }
+}
+
+Console.WriteLine("Исходный массив: ");
+int[,] array = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+printArr(array);
+int[,] x = Array(array);
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив: ");
+printArr(x);
