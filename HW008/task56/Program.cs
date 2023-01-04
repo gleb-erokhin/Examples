5b1cf2a5a05974359df756740bbbc197d8626700
gleb-erokhin <gleb.erokhin@gmail.com>
2023-01-04 20:43:25 +0300

    task56 is done on stable array

--- HW008/task56/Program.cs
+++ HW008/task56/Program.cs
@@ -1,29 +1,81 @@
-﻿int[,] array = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}, {5, 2, 6, 7}};
-int[] summ = new int[4];
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
 
-for(int row = 0; row < array.GetLength(0); row++)
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[] ArraySumm(int[,] array)
 {
-    int sum = 0;
-    for(int col = 0; col < array.GetLength(1); col++)
+    int[] summ = new int[4];
+
+    for(int row = 0; row < array.GetLength(0); row++)
     {
-        sum = sum + array[row, col];
-        // Console.WriteLine($"row = [{row}], col = [{col}], sum - {sum}, array - {array[row, col]}");
-        // Вывод в консоль для тестирования
+        int sum = 0;
+        for(int col = 0; col < array.GetLength(1); col++)
+        {
+            sum = sum + array[row, col];
+            // Console.WriteLine($"row = [{row}], col = [{col}], sum - {sum}, array - {array[row, col]}");
+            // Вывод в консоль для тестирования
+        }
+        summ[row] = sum;
+        // Console.WriteLine();
+        // Console.WriteLine($"summ - {summ[row]}");
+        //  вывод в консоль для тестирования
     }
-    summ[row] = sum;
-    // Console.WriteLine();
-    // Console.WriteLine($"summ - {summ[row]}");
-    //  вывод в консоль для тестирования
+    return summ;
 }
 
+
+void AnswerLine(int[] a)
+{
+    int min = a[0];
+    int index = 0;
+    for(int i = 0; i < a.Length; i++)
+    {
+        if(a[i] < min)
+        {
+            min = a[i];
+            index = i;
+        }
+    }
+    Console.WriteLine($"Номер строки с наименьшей суммой элементов: {index + 1} строка");
+}
+
+void PrintSumm(int[] a)
+{
+    for(int i = 0; i < a.Length; i++)
+    Console.Write($" {a[i]} ");
+}
+
+// ------ Обращение к методу --------
+
+int[,] array = {{8, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}, {5, 2, 6, 7}};
+
+// Получаем одномерный массив с суммой строк
+int[] x = ArraySumm(array);
+
+// Вычисляем строку с наименьшей суммой и выводим на экран
+AnswerLine(x);
+
 Console.WriteLine();
-for(int i = 0; i < summ.Length; i++)
-Console.Write($" {summ[i]} ");
+Console.WriteLine("Массив с суммой строк: ");
+// Печатаем массив для сравнения
+PrintSumm(x);
 
 
 
 
-        // for(int i = 0; i < array.GetLength(1) - 1; i++)
-        // {
-            
-        // }
+
+
+
+
+
+
+// Console.WriteLine();
+// for(int i = 0; i < summ.Length; i++)
+// Console.Write($" {summ[i]} ");
+
