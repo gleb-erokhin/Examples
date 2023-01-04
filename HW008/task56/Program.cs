f47174e6dc6b6ba40be42ab0ec78442892a97501
gleb-erokhin <gleb.erokhin@gmail.com>
2023-01-04 20:53:52 +0300

    task56 is done

--- HW008/task56/Program.cs
+++ HW008/task56/Program.cs
@@ -8,15 +8,31 @@
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[] ArraySumm(int[,] array)
+
+int[,] RandomArray(int a, int b)  // метод создания случайного двумерного массива
 {
-    int[] summ = new int[4];
+    int[,] rand = new int[a, b];
+    for(int row = 0; row < rand.GetLength(0); row++)
+    {
+        for(int col = 0; col < rand.GetLength(1); col++)
+        {
+            rand[row, col] = new Random().Next(1, 10);
+        }
+    }
+    return rand;
+}
+
+
+int[] ArraySumm(int[,] array, int n)  // На вход метода подаем слуяайный двумерный массив и количество строк в массиве, получаем одномерный массив с суммами строк.
+{
+    int[] summ = new int[n];
 
     for(int row = 0; row < array.GetLength(0); row++)
     {
         int sum = 0;
         for(int col = 0; col < array.GetLength(1); col++)
         {
+            // суммируем каждый элемент строки
             sum = sum + array[row, col];
             // Console.WriteLine($"row = [{row}], col = [{col}], sum - {sum}, array - {array[row, col]}");
             // Вывод в консоль для тестирования
@@ -30,7 +46,7 @@ int[] ArraySumm(int[,] array)
 }
 
 
-void AnswerLine(int[] a)
+void AnswerLine(int[] a)  // вычисляем минимальный элемент в массиве и его индекс
 {
     int min = a[0];
     int index = 0;
@@ -45,7 +61,7 @@ void AnswerLine(int[] a)
     Console.WriteLine($"Номер строки с наименьшей суммой элементов: {index + 1} строка");
 }
 
-void PrintSumm(int[] a)
+void PrintSumm(int[] a)  // печать одномерного массива с суммами для сравнения и отладки
 {
     for(int i = 0; i < a.Length; i++)
     Console.Write($" {a[i]} ");
@@ -53,18 +69,26 @@ void PrintSumm(int[] a)
 
 // ------ Обращение к методу --------
 
-int[,] array = {{8, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}, {5, 2, 6, 7}};
+// Вводим количество строк и столбцов
+Console.WriteLine("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-// Получаем одномерный массив с суммой строк
-int[] x = ArraySumm(array);
+// Вводим значения в метод для создания случайного двумерного массива и печатаем его
+int[,] x = RandomArray(n, m);
+
+
+// Получаем одномерный массив с суммой строк, дополнительно передаем количество строк
+int[] y = ArraySumm(x, n);
 
 // Вычисляем строку с наименьшей суммой и выводим на экран
-AnswerLine(x);
+AnswerLine(y);
 
+// Печатаем массив для сравнения
 Console.WriteLine();
 Console.WriteLine("Массив с суммой строк: ");
-// Печатаем массив для сравнения
-PrintSumm(x);
+PrintSumm(y);
 
 
 
